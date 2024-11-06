418d7c70211eb8744f85676ba0a5c81632e645b8
Nikita-Samoilenko <mykyta.samoilenko@gmail.com>
2024-11-06 17:26:29 +0200

    Filters

---
+++ src/js/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://your-energy.b.goit.study/api',
+  headers: {
+    Accept: 'application/json',
+  },
+});
+
+export const fetchFilters = async filterType => {
+  try {
+    const response = await api.get('/filters', {
+      params: {
+        filter: filterType,
+        page: 1,
+        limit: 12,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching filters:', error);
+  }
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import { fetchFilters } from './api.js';
+
 document.addEventListener('DOMContentLoaded', () => {
   const menuButton = document.querySelector('.burger-menu');
   const menu = document.querySelector('.menu');
@@ -27,4 +29,22 @@ document.addEventListener('DOMContentLoaded', () => {
   menuBackdrop.addEventListener('click', toogleMenu);
   closeMenuButton.addEventListener('click', toogleMenu);
   mobileNavigation.addEventListener('click', toogleMenu);
+
+  const initializeFilterListener = () => {
+    const filters = document.getElementById('filters');
+    const buttons = filters.querySelectorAll('.filter-button');
+
+    filters.addEventListener('click', event => {
+      const button = event.target;
+
+      if (button.classList.contains('filter-button')) {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        fetchFilters(button.getAttribute('data-filter'));
+      }
+    });
+  };
+
+  initializeFilterListener();
 });
