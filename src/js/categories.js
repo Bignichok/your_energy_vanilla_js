936c2405a8aefcfe2e3d2f6df4dc4f3213d37821
Nikita-Samoilenko <mykyta.samoilenko@gmail.com>
2024-11-08 17:19:39 +0200

    Exercises section

--- src/js/categories.js
+++ src/js/categories.js
@@ -51,6 +51,44 @@ export const initializeCategoriesSection = () => {
     cb && cb();
   };
 
+  const getExercisesMarkup = exercises => {
+    return exercises
+      .map(
+        ({ rating, name, bodyPart, target, burnedCalories }) => `
+          <li class="exercise-list-item">
+      <div class="first-row">
+        <div class="workout-element">WORKOUT</div>
+        <div class="rating-holder">
+          <span>${parseFloat(rating).toFixed(1)}</span>
+          <svg width="18" height="18">
+            <use href="./img/sprite.svg#icon-star"></use>
+          </svg>
+        </div>
+        <button class="start-button">
+          Start
+          <svg width="16" height="16">
+            <use href="./img/sprite.svg#icon-arrow-right"></use>
+          </svg>
+        </button>
+      </div>
+      <div class="second-row">
+        <svg width="24" height="24">
+          <use href="./img/sprite.svg#icon-running-black"></use>
+        </svg>
+        <p>${name}</p>
+      </div>
+
+      <ul class="exercise-description-list">
+        <li><span>Burned calories:</span> ${burnedCalories} / 3 min</li>
+        <li><span>Body part:</span> ${bodyPart}</li>
+        <li><span>Target:</span> ${target}</li>
+      </ul>
+    </li>
+        `
+      )
+      .join('');
+  };
+
   const renderExercises = (exercises, category) => {
     exercisesContainer.innerHTML = '';
     categoriesContainer.style.display = 'none';
@@ -59,15 +97,10 @@ export const initializeCategoriesSection = () => {
     scrollToFilters();
 
     if (exercises && exercises.length > 0) {
-      exercises.forEach(exercise => {
-        const exerciseItemHTML = `
-          <li class="exercise-list-item">
-            <p class="exercise-list-item-title">${exercise.name}</p>
-            <p class="exercise-list-item-details">${exercise.details}</p>
-          </li>
-        `;
-        exercisesContainer.insertAdjacentHTML('beforeend', exerciseItemHTML);
-      });
+      exercisesContainer.insertAdjacentHTML(
+        'beforeend',
+        getExercisesMarkup(exercises)
+      );
     } else {
       exercisesContainer.insertAdjacentHTML(
         'beforeend',
