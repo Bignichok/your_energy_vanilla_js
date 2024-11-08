15e1167535714928c101c065b22c3e411549c3ba
Nikita-Samoilenko <mykyta.samoilenko@gmail.com>
2024-11-08 22:23:05 +0200

    divide by different js files

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -1,7 +1,13 @@
 import { fetchCategories, fetchExercises } from './api.js';
 import { Pagination } from './Pagination.js';
+import {
+  renderTitle,
+  getCategoriesMarkup,
+  getExercisesMarkup,
+} from './markupUtils.js';
+import { toggleClearButton, clearInput, scrollToFilters } from './utils.js';
 
-export const initializeCategoriesSection = () => {
+export const initializeExerceisesSection = () => {
   const categoriesContainer = document.querySelector('.categories-list');
   const filtersTitle = document.querySelector('.filters-title');
   const filters = document.querySelector('#filters');
@@ -17,15 +23,13 @@ export const initializeCategoriesSection = () => {
   let currentCategory = '';
   let currentFilter = '';
 
-  const toggleClearButton = () => {
-    clearButton.style.display = searchInput.value ? 'flex' : 'none';
-  };
-
-  const clearInput = () => {
-    searchInput.value = '';
-    toggleClearButton();
-    searchInput.focus();
-  };
+  searchInput.addEventListener('input', () =>
+    toggleClearButton(searchInput, clearButton)
+  );
+  clearButton.addEventListener('click', () =>
+    clearInput(searchInput, clearButton)
+  );
+  searchForm.addEventListener('submit', event => handleFormSubmit(event));
 
   const handleFormSubmit = event => {
     event.preventDefault();
@@ -38,92 +42,22 @@ export const initializeCategoriesSection = () => {
     });
   };
 
-  searchInput.addEventListener('input', toggleClearButton);
-  clearButton.addEventListener('click', clearInput);
-  searchForm.addEventListener('submit', handleFormSubmit);
-
-  const scrollToFilters = () => {
-    window.scrollTo({ top: filtersTitle.offsetTop, behavior: 'smooth' });
-  };
-
-  const renderTitle = category => {
-    filtersTitle.innerHTML = category
-      ? `Exercises /<span>${category}</span>`
-      : 'Exercises';
-  };
-
-  const getCategoriesMarkup = categories => {
-    return categories
-      .map(
-        ({ filter, name, imgURL }) => `
-        <li class="categories-list-item" data-filter="${filter}" data-name="${name}"
-          style="background-image: linear-gradient(0deg, rgba(17, 17, 17, 0.5), rgba(17, 17, 17, 0.5)), url(${imgURL})">
-            <p class="categories-list-item-title">${name}</p>
-            <p class="categories-list-item-sub-title">${filter}</p>
-          </li>
-      `
-      )
-      .join('');
-  };
-
   const renderCategories = (categories, cb) => {
     categoriesContainer.innerHTML = '';
     categoriesContainer.style.display = 'grid';
     exercisesContainer.style.display = 'none';
     searchForm.style.display = 'none';
-    renderTitle();
-    scrollToFilters();
+    renderTitle(filtersTitle, null);
+    scrollToFilters(filtersTitle);
 
-    if (categories && categories.length > 0) {
-      categoriesContainer.insertAdjacentHTML(
-        'beforeend',
-        getCategoriesMarkup(categories)
-      );
-    } else {
-      categoriesContainer.insertAdjacentHTML(
-        'beforeend',
-        '<p>No categories found for the selected filter.</p>'
-      );
-    }
+    categoriesContainer.insertAdjacentHTML(
+      'beforeend',
+      categories && categories.length
+        ? getCategoriesMarkup(categories)
+        : '<p>No categories found for the selected filter.</p>'
+    );
+
     cb && cb();
-  };
-
-  const getExercisesMarkup = exercises => {
-    return exercises
-      .map(
-        ({ rating, name, bodyPart, target, burnedCalories }) => `
-          <li class="exercise-list-item">
-      <div class="first-row">
-        <div class="workout-element">WORKOUT</div>
-        <div class="rating-holder">
-          <span>${parseFloat(rating).toFixed(1)}</span>
-          <svg width="18" height="18">
-            <use href="./img/sprite.svg#icon-star"></use>
-          </svg>
-        </div>
-        <button class="start-button">
-          Start
-          <svg width="16" height="16">
-            <use href="./img/sprite.svg#icon-arrow-right"></use>
-          </svg>
-        </button>
-      </div>
-      <div class="second-row">
-        <svg width="24" height="24">
-          <use href="./img/sprite.svg#icon-running-black"></use>
-        </svg>
-        <p>${name}</p>
-      </div>
-
-      <ul class="exercise-description-list">
-        <li><span>Burned calories:</span> ${burnedCalories} / 3 min</li>
-        <li><span>Body part:</span> ${bodyPart}</li>
-        <li><span>Target:</span> ${target}</li>
-      </ul>
-    </li>
-        `
-      )
-      .join('');
   };
 
   const renderExercises = (exercises, category) => {
@@ -131,20 +65,15 @@ export const initializeCategoriesSection = () => {
     categoriesContainer.style.display = 'none';
     exercisesContainer.style.display = 'grid';
     searchForm.style.display = 'flex';
-    renderTitle(category);
-    scrollToFilters();
+    renderTitle(filtersTitle, category);
+    scrollToFilters(filtersTitle);
 
-    if (exercises && exercises.length > 0) {
-      exercisesContainer.insertAdjacentHTML(
-        'beforeend',
-        getExercisesMarkup(exercises)
-      );
-    } else {
-      exercisesContainer.insertAdjacentHTML(
-        'beforeend',
-        '<p>No exercises found for the selected category and filter.</p>'
-      );
-    }
+    exercisesContainer.insertAdjacentHTML(
+      'beforeend',
+      exercises && exercises.length
+        ? getExercisesMarkup(exercises)
+        : '<p>No exercises found for the selected category and filter.</p>'
+    );
   };
 
   const setActiveFilterButton = filterName => {
