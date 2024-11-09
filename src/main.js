f33b9df842de80b754f4140b3327cb6b08d68e05
Nikita-Samoilenko <mykyta.samoilenko@gmail.com>
2024-11-09 15:42:50 +0200

    Favorites base layout

--- src/main.js
+++ src/main.js
@@ -1,36 +1,11 @@
 import { initializeExercisesSection } from './js/exercises.js';
 import { displayQuoteOfTheDay } from './js/quote.js';
 import { initializeExerciseModal } from './js/exerciseModal.js';
+import { initializeMenu, initializeNavigationLinks } from './js/menu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const menuButton = document.querySelector('.burger-menu');
-  const menu = document.querySelector('.menu');
-  const menuBackdrop = document.querySelector('.menu-backdrop');
-  const closeMenuButton = document.querySelector('.close-menu-button');
-  const mobileNavigation = document.querySelector('.mobile-nav-list');
-  const links = document.querySelectorAll('.nav-link');
-
-  const toggleMenu = () => {
-    menu.classList.toggle('active');
-    menuBackdrop.classList.toggle('active');
-    menuButton.classList.toggle('active');
-  };
-
-  const currentPath = window.location.pathname.split('/').pop() || 'index.html';
-
-  const isActiveLink = linkPath => linkPath === currentPath;
-
-  links.forEach(link => {
-    if (isActiveLink(link.getAttribute('href'))) {
-      link.classList.add('active');
-    }
-  });
-
-  menuButton.addEventListener('click', toggleMenu);
-  menuBackdrop.addEventListener('click', toggleMenu);
-  closeMenuButton.addEventListener('click', toggleMenu);
-  mobileNavigation.addEventListener('click', toggleMenu);
-
+  initializeMenu();
+  initializeNavigationLinks();
   initializeExercisesSection();
   displayQuoteOfTheDay();
   initializeExerciseModal();
